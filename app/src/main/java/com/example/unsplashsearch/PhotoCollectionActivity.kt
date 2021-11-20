e5b6639d2bc303da3c03c673038ead5499746003
Hyunsoo Song <sj960126@naver.com>
2021-11-21 01:35:34 +0900

    fix : 데이터가 없는 경우 예외처리

--- app/src/main/java/com/example/unsplashsearch/PhotoCollectionActivity.kt
+++ app/src/main/java/com/example/unsplashsearch/PhotoCollectionActivity.kt
@@ -14,7 +14,7 @@ import com.google.android.material.appbar.MaterialToolbar
 class PhotoCollectionActivity:AppCompatActivity() {
     //데이터
     private var photoList=ArrayList<Photo>()
-
+    private var isSuccess : Boolean = false
     private val photoGridRecyclerViewAdapter : PhotoGridRecyclerViewAdapter by lazy {
         PhotoGridRecyclerViewAdapter(photoList) //그냥 이런식으로 넘기는거 추천 
     }
@@ -31,10 +31,11 @@ class PhotoCollectionActivity:AppCompatActivity() {
         if (intent.hasExtra("array_bundle")){
             val bundle=intent.getBundleExtra("array_bundle")//번들 받아서 저장
             photoList= bundle?.getSerializable("photo_array_list") as ArrayList<Photo>//받은 번들안에있는 요청 결과를 photoList에 저장
+            isSuccess = true
         }
         if (intent.hasExtra("array_bundle")){
             val search_term = intent.getStringExtra("search_term")//받은 검색어 저장
-        }      
+        }
     }
     
     private fun setBinding(){
@@ -44,10 +45,15 @@ class PhotoCollectionActivity:AppCompatActivity() {
     }
     
     private fun setRecyclerview(){
-        my_photo_recycler_view.apply{
+        if(isSuccess){
+            my_photo_recycler_view.apply{
             layoutManager=GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false)
             adapter=this.photoGridRecyclerViewAdapter
+        }else {
+            //데이터가 안 넘어오는 경우 앱이 터짐 이런 경우 예외로 이전 스택으로 이동한는 코드 
         }
+       
+        
     }
    
 }
