ee52732837fa87d28d3c031b187bc11ded95f722
HwanHee97 <frog7005@gmail.com>
2021-11-23 14:35:32 +0900

    refactor: 사진 리사이클러뷰 데이터 바인딩방식으으로 변환/이미지 어댑터 리팩토링

--- app/src/main/java/com/example/unsplashsearch/model/Photo.kt
+++ app/src/main/java/com/example/unsplashsearch/model/Photo.kt
@@ -13,15 +13,15 @@ data class Photo(
     var createAt: String?,//생성일
     var likesCount: Int?//좋아요 눌린수
 ):Serializable {
-    object ImgBind{
-        @JvmStatic
-        @BindingAdapter("setPhotoImage")
-        fun loadPhotoImage(view: ImageView, imageUrl: String) {
-            Glide.with(App.instance)
-                .load(imageUrl)//표시할 이미지 값(링크).
-                .placeholder(R.drawable.ic_baseline_insert_photo_24)//이미지가 없으면 나오는 기본 화면
-                .into(view)//표시할 이미지 뷰
-
-        }
-    }
+//    object ImgBind{
+//        @JvmStatic
+//        @BindingAdapter("setPhotoImage")
+//        fun loadPhotoImage(view: ImageView, imageUrl: String) {
+//            Glide.with(App.instance)
+//                .load(imageUrl)//표시할 이미지 값(링크).
+//                .placeholder(R.drawable.ic_baseline_insert_photo_24)//이미지가 없으면 나오는 기본 화면
+//                .into(view)//표시할 이미지 뷰
+//
+//        }
+//    }
 }
--- app/src/main/java/com/example/unsplashsearch/recyclerview/PhotoItemViewHolder.kt
+++ app/src/main/java/com/example/unsplashsearch/recyclerview/PhotoItemViewHolder.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.model.GlideUrl
 import com.example.unsplashsearch.App
 import com.example.unsplashsearch.R
 import com.example.unsplashsearch.databinding.ActivityPhotoCollectionBinding
@@ -19,11 +20,21 @@ import com.example.unsplashsearch.utils.Constants
 class PhotoItemViewHolder(val binding:LayoutPhotoItemBinding ) :RecyclerView.ViewHolder(binding.root) {
 
     //데이터바인딩 사용하여 데이터를 넘김
-    fun bindWithView(Item:Photo){
+    fun bindWithView(item:Photo){
         Log.d(Constants.TAG,"PhotoItemViewHolder - bindWithView() called")
-            binding.photoItem=Item
-            //이미지는 일반 바인딩으로는 할수없어서 Photo클래스 안에 이미지 바인딩 어댑터함수를 선언했음 xml파일에서 함수이름=@{이미지url} 을 선언해줌
+            //이미지만을 위한 어댑터를  Photo클래스 안에 이미지 바인딩 어댑터함수를 따로 선언하고 xml파일에서 함수이름=@{이미지url} 을 선언히여 사용가능
+            with(binding){
+                photoItem=item
+                loadPhotoImage(photoImage,item.thumbnail)
+            }
     }
 
+    fun loadPhotoImage(view: ImageView, imageUrl: String?) {
+        Glide.with(App.instance)
+            .load(imageUrl)//표시할 이미지 값(링크).
+            .placeholder(R.drawable.ic_baseline_insert_photo_24)//이미지가 없으면 나오는 기본 화면
+            .into(view)//표시할 이미지 뷰
+
+    }
 
 }
